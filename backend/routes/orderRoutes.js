6bc7126a6d3e74dee283f4a795114382a50e747f
Devansh Gupta <devansh.g6012@gmail.com>
2024-05-09 23:45:48 +0530

    Initial commit

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router()
+import { addOrderItems, getMyOrders, getOrderById, getOrders } from "../controllers/orderController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id').get(protect, admin, getOrderById);
+
+export default router
